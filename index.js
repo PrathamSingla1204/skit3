590f5c1d330e0148d493c196bf65bf5978e2ec39
Pratham <Pratham.singla.762@gmail.com>
2023-07-19 02:24:48 +0530

    Project completed

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const port = 8000;
 const db = require('./config/mongoose');
 
-
-app.use('/products', require('./routes'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/products', require('./routes'));
+
 app.listen(port,(err)=>{
     if(err){
         console.log(err);
